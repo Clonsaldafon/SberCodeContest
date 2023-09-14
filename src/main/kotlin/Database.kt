15aa06c7b6149566a142484e170aa894443e5a3e
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-09-14 18:26:12 +0500

    Health check on test data

--- src/main/kotlin/Database.kt
+++ src/main/kotlin/Database.kt
@@ -1,6 +1,28 @@
 class Database {
-    private var categories: List<Category> = mutableListOf();
-    private var features: List<Feature> = mutableListOf();
+    private var categories: List<Category> = mutableListOf(
+        Category(5, "Category_5"),
+        Category(3, "Category_3"),
+        Category(1, "Category_1"),
+        Category(2, "Category_2"),
+        Category(4, "Category_4")
+    );
+
+    private var features: List<Feature> = mutableListOf(
+        Feature(8, 3, "Feature_8", 50),
+        Feature(4, 2, "Feature_4", 350),
+        Feature(2, 4, "Feature_2", 150),
+        Feature(14, 3, "Feature_14", 450),
+        Feature(3, 5, "Feature_3", 520),
+        Feature(7, 4, "Feature_7", 50),
+        Feature(13, 3, "Feature_13", 550),
+        Feature(5, 1, "Feature_5", 250),
+        Feature(9, 2, "Feature_9", 550),
+        Feature(1, 1, "Feature_1", 560),
+        Feature(12, 1, "Feature_12", 50),
+        Feature(6, 3, "Feature_6", 500),
+        Feature(11, 5, "Feature_11", 540),
+        Feature(10, 3, "Feature_10", 50),
+    );
 
     fun getCategories(): List<Category> {
         return categories;
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,22 @@
 fun main(args: Array<String>) {
     val db: Database = Database();
 
-    val categories: List<Category> = db.getCategories();
-    val features: List<Feature> = db.getFeatures();
+    var categories: List<Category> = db.getCategories();
+    var features: List<Feature> = db.getFeatures();
 
-    categories.sortedBy { it.categoryId };
-    features.sortedBy { it.featureId };
+    categories = categories.sortedBy { it.categoryId };
+    features = features.sortedBy { it.featureId };
 
     val mergedList: List<Any> = mergeCategoriesAndFeatures(categories, features);
+
+    for (it in mergedList) {
+        if (it is Category) {
+            println("Category: " + it.name);
+        }
+        else if (it is Feature) {
+            println("Feature: " + it.title);
+        }
+    }
 }
 
 fun mergeCategoriesAndFeatures(categories: List<Category>, features: List<Feature>): List<Any> {
